ef746aa1334515d98a4b519dc2c5a711935f6559
Errol Wallace <ewally24@gmail.com>
2016-07-25 04:40:35 -0400

    Added Parallax Functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+	$(window).bind('scroll',function(e){
+    	parallaxScrolling();
+    });
+    
 });
+
+function parallaxScrolling() {
+	var scrolled = $(window).scrollTop();
+	$('#parallax-bg1').css('top', (0-(scrolled *.25)) + 'px');
+}
